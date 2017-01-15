527e11a90d779a142c9342f366fe854ad05b9262
PATRICK <patrick@PATRICKs-MacBook-Pro.local>
2017-01-15 15:56:50 +0000

    hello world

---
+++ helloworld2/helloworld2/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  helloworld2
+//
+//  Created by PATRICK on 15/01/2017.
+//  Copyright © 2017 PATRICK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    
+        print("Hello World")
+    }
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
